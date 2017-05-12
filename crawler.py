cc39f1d71165c782b0bed7f6ab81dd767f947202
pauluskim <jkkim9103@gmail.com>
2017-05-12 17:44:57 +0900

    crawl bunch of cetizen pages

--- crawler.py
+++ crawler.py
@@ -1,17 +1,35 @@
 # -*- coding: utf-8 -*-
 # 한글을 못읽어 드리는 파이썬을 위한 ㅠㅠ
 
+
+
 import scrapy
 import MySQLdb
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class cetizenSpider(scrapy.Spider):
+    name = "cetizen"
     start_urls = [
-        'http://market.cetizen.com/market.php?q=view&auc_no=16973117&auc_sale=1&just_one=&just_one_name=&auc_wireless=&auc_make=&auc_price1=&auc_price2=&akeyword=&keyword_p=%BE%C6%C0%CC%C6%F96&auc_img=&kfild=&hotopt=&dealer=&quality=&usim=&asdate=&nowpage=&escrow_motion=3&view_type=&p=1',
+        'http://market.cetizen.com/market.php?q=market&auc_sale=1&escrow_motion=3&sc=1&qs=&keyword_p=%BE%C6%C0%CC%C6%F96',
     ]
 
+
+    def start_requests(self):
+        iphone_main_url = "http://market.cetizen.com/market.php?q=market&auc_sale=1&escrow_motion=3&sc=1&qs=&keyword_p=%BE%C6%C0%CC%C6%F96"
+        yield scrapy.Request(url=iphone_main_url, callback=self.main_page_parse)
+
+
+    def main_page_parse(self, response):
+        visit_page_list = response.css(".clr100>a::attr('href')").extract()
+
+        for url in visit_page_list:
+            yield scrapy.Request(url="http://market.cetizen.com"+url, callback=self.parse)
+
     def parse(self, response):
+        """
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').extract_first(),
@@ -22,6 +40,29 @@ class QuotesSpider(scrapy.Spider):
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+        """
+        title = response.css(".p17.b.p17.b::text").extract_first()
+        spec = response.css(".p15 span::text").extract_first()
+        price = int(response.css(".p21::text").extract_first().strip().replace(",",""))
+        detail = {"title": title, "spec": spec, "price": price}
+        self.save(detail)
+
+    def save(self, detail):
+        # Open database connection
+        db = MySQLdb.connect("localhost", "jack", password, "roka")
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor(MySQLdb.cursors.DictCursor)
+        # MySQLdb.cursors.DictCursor를 사용하는 이유 :  query를 통해 데이터를 얻을때 python의 dict로 얻기 위함
+
+        myDict = {'model': detail["title"], 'price': detail["price"], 'extra': detail["spec"]}
+
+        columns = ','.join(myDict.keys())
+        placeholders = ','.join(['%s'] * len(myDict))
+        query = "insert into %s (%s) values (%s)" % ("raw_price", columns, placeholders)
+        cursor.execute(query, myDict.values())
+        db.commit()
+        db.close()
 
 
 
@@ -29,24 +70,6 @@ class QuotesSpider(scrapy.Spider):
 
 
 
-
-
-# Open database connection
-db = MySQLdb.connect("localhost","jack",password,"roka" )
-
-# prepare a cursor object using cursor() method
-cursor = db.cursor(MySQLdb.cursors.DictCursor)
-# MySQLdb.cursors.DictCursor를 사용하는 이유 :  query를 통해 데이터를 얻을때 python의 dict로 얻기 위함
-
-myDict = {'model': 'iphone5', 'price': '40000', 'extra': 'good'}
-
-
-columns = ','.join(myDict.keys())
-placeholders = ','.join(['%s'] * len(myDict))
-query = "insert into %s (%s) values (%s)" % ("raw_price", columns, placeholders )
-cursor.execute(query, myDict.values())
-db.commit()
-db.close()
 
 """
 Select!!
